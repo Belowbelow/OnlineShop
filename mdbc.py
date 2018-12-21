77bc8ead052ddd5e69952b4844595e4ab878e4f0
starsky <1458531219@qq.com>
2018-12-21 12:59:44 +0800

    数据库测试

--- mdbc.py
+++ mdbc.py
@@ -4,13 +4,12 @@ conn = mysql.connector.connect(user='root', password='123456', database='test')
 cursor = conn.cursor()
 
 #cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
-cursor.execute('insert into user (id, name) values (%s, %s)', ['001', 'Alice'])
+#cursor.execute('insert into user (id, name) values (%s, %s)', ['001', 'Alice'])
 print(cursor.rowcount)
 cursor.execute('select * from user')
+print(cursor.execute('select * from user'))
 
 
 cursor.close()
 conn.commit()
-
-cursor = conn.cursor()
-cursor.execute('select * from user')
+conn.close()
