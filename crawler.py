a448c663fdf40164ec2ad82476ae4e80eec0777d
starsky <1458531219@qq.com>
2019-01-03 18:14:02 +0800

    爬取图书信息并上传到数据库

--- crawler.py
+++ crawler.py
@@ -1,45 +1,98 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import requests
-import chardet
+import chardet, random
+import io, sys, codecs
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
 
 def getSoup(url):
     try:
-        page = urlopen(url)
-        print(page.read().decode('gbk'))
-        return BeautifulSoup(page.read(), features='lxml')
+        page = urlopen(url, timeout=10).read()
+        if page[:3] == codecs.BOM_UTF8:
+            page = page[3:]
+        return BeautifulSoup(page, "html.parser")
     except BaseException as e:
         print(e)
 
-def getFirstPage(url):
+def change(authors):
+    a = ''
+    for i in authors:
+        a = a + i
+    return(a)
+
+#print(change(['aa', 'bb', 'cc']))
+
+def change1(info):
+    return "'" + info + "'"
+
+def save(num, bookname, price, author, introduction):
+    f_path = "book.txt" 
+    content = []
+    with open(f_path, 'a', encoding='utf-8') as f:
+        b_type = '计算机科学'
+        store = random.randint(12, 15)
+        sold = random.randint(2, 10)
+        pic = "./pic/book_" + str(num) + ".jpg"
+
+        data = change1(str(num)) + ',' + change1(bookname) + ',' + change1(str(price)) + ',' + change1(b_type) + ',' + change1(str(store)) + ',' + change1(str(sold)) + ',' + change1(change(author)) + ',' + change1(str(introduction)) + ',' + change1(pic)
+        f.write(data + '\n')
+
+def getFirstPage(url, num=1001):
     soup = getSoup(url)
-    if soup:
-        print('I get the page')
-        print(len(soup))
-        bookpics = []
-        bookname = []
-        #bookscore = []
-        bookauthor = []
-        bookpublisher = []
-        bookprice = []
-        bookblocks = soup.select("ul[class='bang_list clearfix bang_list_mode']")
+    books = soup.select('div[class="xians-list"]')[0].select('ul')[0].select("li")
+    book_images = []
+    book_name = []
+    book_author = []
+    book_publisher = []
+    book_price = []
 
-        for bookblock in bookblocks:
-            bookpics.append(bookblock.select("div[class='pic']"))[0].a.get("href")
-            bookname.append(bookblock.select("div[class='name']"))[0].a.get_text()
-            #bookscore.append(bookblock.select("div[class='star']"))[0]
-            bookauthor.append(bookblock.select("div[class='publisher_info']"))[0].a.get_text()
-            bookpublisher.append(bookblock.select("div[class='publisher_info']"))[1].a.get_text()
-            bookprice.append(bookblock.select("div[clas='price']"))[0].p.span.get_text()
-        
-        for (pic, name, author, publisher, price) in zip(bookpics, bookname, bookauthor, bookpublisher, bookprice):
-            print(pic, name, author, publisher, price)
-        
-    else:
-        print("Get nothing")
+    for book in books:
+        images = book.table.select('tr')[0].td.a.select('img')[0].get('src')
+        book_images.append(images)
 
-url = "http://bang.dangdang.com/books/enewhotbooks/98.01.00.00.00.00-recent7-0-0-1-1"
-getFirstPage(url)
+        name = book.table.select('tr')[1].td.a.get_text().replace('[按需印刷]', '').replace('[即时配发]', '')
+        book_name.append(name)
 
-# p = urlopen(url)
-# print(p.read())
+        authors = []
+        for i in book.table.select('tr')[2].td.select('a'):
+            authors.append(i.get_text() + ' ')
+        book_author.append(authors[:-1])
+
+        book_publisher.append(authors[-1])
+
+        price = book.table.select('tr')[4].select('td')[1].get_text().replace('\n', '').replace('\r', '').replace(' ', '')
+        book_price.append(price.replace('定价：￥', ''))
+
+    for x, y, z, p, l in zip(book_name, book_price, book_author, book_publisher, book_images):
+        save(num, x, y, z, p)
+        # r = requests.get(l)
+        # imgPath = './pic/' + str(num) + '.jpg'
+        # with open(imgPath, 'wb') as p:
+        #     p.write(r.content)
+        num = num + 1
+
+
+urls = ["http://product.china-pub.com/cache/rank3/ebook/day/rank_day_7_59_1.html",
+        "http://product.china-pub.com/cache/rank3/ebook/day/rank_day_7_59_2.html",
+        "http://product.china-pub.com/cache/rank3/ebook/day/rank_day_7_59_4.html",
+        "http://product.china-pub.com/cache/rank3/ebook/day/rank_day_7_59_5.html"]
+
+nums = [1001, 1021, 1041, 1061]
+
+def main():
+    for (url, num) in zip(urls, nums):
+        try:
+            print("Get info......")
+            getFirstPage(url, num)
+        except BaseException as e:
+            print("Error: ", e)
+
+def check():
+    fPath = './book.txt'
+    with open(fPath, 'r', encoding='utf-8') as f:
+        for line in f.readlines():
+            print('('+line+')')
+
+main()
+#check()
--- createdb.py
+++ createdb.py
@@ -4,12 +4,12 @@ def create_user(cur):
     cmd = '''
     CREATE TABLE IF NOT EXISTS users 
     (id INT NOT NULL AUTO_INCREMENT,
-    username varchar(20) NOT NULL,
-    password varchar(20) NOT NULL,
-    email varchar(20) NOT NULL,
-    phonenumber varchar(20) NOT NULL,
+    username varchar(50) NOT NULL,
+    password varchar(50) NOT NULL,
+    email varchar(50) NOT NULL,
+    phonenumber varchar(50) NOT NULL,
     address varchar(50) NOT NULL,
-    bankcard varchar(20) NOT NULL,
+    bankcard varchar(50) NOT NULL,
     PRIMARY KEY (id));
     '''
     cur.execute(cmd)
@@ -18,15 +18,15 @@ def create_book(cur):
     cmd = '''
     CREATE TABLE IF NOT EXISTS books 
     (bookid INT NOT NULL AUTO_INCREMENT,
-    ISBN varchar(20) NOT NULL,
-    bookname varchar(20) NOT NULL,
-    price varchar(20) NOT NULL,
-    type varchar(20) NOT NULL,
-    storehouse varchar(20) NOT NULL,
-    soldout varchar(20) NOT NULL,
-    author varchar(20) NOT NULL,
+    ISBN varchar(50) NOT NULL,
+    bookname varchar(50) NOT NULL,
+    price varchar(50) NOT NULL,
+    type varchar(50) NOT NULL,
+    storehouse varchar(50) NOT NULL,
+    soldout varchar(50) NOT NULL,
+    author varchar(50) NOT NULL,
     introduce varchar(255) NOT NULL,
-    bookpicture varchar(30) NOT NULL,
+    bookpicture varchar(50) NOT NULL,
     PRIMARY KEY (bookid))
     '''
     cur.execute(cmd)
@@ -35,13 +35,25 @@ def create_order(cur):
     cmd = '''
     CREATE TABLE IF NOT EXISTS orders 
     (orderid INT NOT NULL AUTO_INCREMENT,
-    customer varchar(20) NOT NULL,
-    ISBN varchar(20) NOT NULL,
-    number varchar(20) NOT NULL,
-    state varchar(20) NOT NULL,
+    customer varchar(50) NOT NULL,
+    ISBN varchar(50) NOT NULL,
+    number varchar(50) NOT NULL,
+    state varchar(50) NOT NULL,
     PRIMARY KEY(orderid))
     '''
     cur.execute(cmd)
+
+def insert_books(cur):
+    cmd = '''
+    INSERT INTO books
+    (ISBN, bookname, price, type, storehouse, soldout, author, introduce, bookpicture)
+    VALUES
+    '''
+    fPath = './book.txt'
+    with open(fPath, 'r', encoding='utf-8') as f:
+        for line in f.readlines():
+            print('(' + str(line).replace('\r', '').replace('\n', '') +');')
+            cur.execute(cmd + '(' + str(line).replace('\r', '').replace('\n', '') +');')
 
 def create():
     conn = mysql.connector.connect(user='root', password='123456', database='bookshop')
@@ -51,6 +63,8 @@ def create():
         create_user(cursor)
         create_book(cursor)
         create_order(cursor)
+        insert_books(cursor)
+
     except BaseException as e:
         print(e)
 
